6b8391477855bfa9c54310028058a7ac5763e208
Jinki Lee <JINKILEE552@GMAIL.COM>
2023-09-23 16:48:40 -0600

    Removed Contact, changed pages.
    Ready for final deployment

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import integritymechanical from '../images/clients/integritymechanical.png';
-import cleanersupply from '../images/clients/cleanersupply.png';
+import integritymechanical from "../images/clients/integritymechanical.png";
+import cleanersupply from "../images/clients/cleanersupply.png";
 
 const clientImage = {
-    height: '10rem',
-    width: 'auto',
-    mixBlendMode: 'colorBurn'
-}
+  height: "10rem",
+  width: "auto",
+  mixBlendMode: "colorBurn",
+};
 
 const Clients = () => {
-    return (
-        <div className="mt-8 bg-gray-300">
-            <section data-aos="fade-up">
-                <div className="my-4 py-4">
-                    <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our Partners</h2>
-                    <div className='flex justify-center'>
-                        <div className='w-24 border-b-4 border-blue-900'></div>
-                    </div>
-                    <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">Some of our partners.</h2>
-                </div>
-
-                <div className="p-16" data-aos="fade-in" data-aos-delay="600">
-                    <div className="grid sm:grid-cols-2 lg:grid-cols-2">
-                        <div style={clientImage} className= "overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/2">
-                            <img src={integritymechanical} alt="client" />                           
-                        </div>
-
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={cleanersupply} alt="client" />                            
-                        </div>                                           
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="mt-8 bg-gray-300">
+      <section data-aos="fade-up">
+        <div className="my-4 py-4">
+          <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
+            Our Partners
+          </h2>
+          <div className="flex justify-center">
+            <div className="w-24 border-b-4 border-blue-900"></div>
+          </div>
         </div>
-    )
-}
+
+        <div className="p-16" data-aos="fade-in" data-aos-delay="600">
+          <div className="grid sm:grid-cols-3 lg:grid-cols-3">
+            <div
+              style={clientImage}
+              className="overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/2"
+            >
+              <img src={integritymechanical} alt="client" />
+            </div>
+
+            <div
+              style={clientImage}
+              className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100"
+            >
+              <img src={cleanersupply} alt="client" />
+            </div>
+            <div className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-100 hover:opacity-100">
+              <div className="text-center">
+                <h2 className="mt-4 text-xl lg:text-l font-semibold text-gray-600 common-class">
+                  This website was developed by Han's son, Jinki Lee
+                </h2>
+                <a
+                  href="https://www.linkedin.com/in/jinkilee/"
+                  target="_blank" // Opens the link in a new tab/window
+                  rel="noopener noreferrer" // Recommended for security
+                  className="common-class text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group"
+                >
+                  View Jinki's LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Clients;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,42 +1,76 @@
-import React from 'react';
-import img from '../images/Web-developer.svg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import img from "../images/Web-developer.svg";
+import { Link } from "react-router-dom";
 
 const Intro = () => {
-    return (
-        <>
-                <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id='about' >
-                <div className="my-4 py-4">
-                        <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our History</h2>
-                        <div className='flex justify-center'>
-                            <div className='w-24 border-b-4 border-blue-900'></div>
-                        </div>
-                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">About Q&P Cleaners</h2>
-                    </div>
-                    
-                    <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
-                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
-                            <img alt="card img" className="rounded-t float-right" src={img} />
-                        </div>
-                        <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
-                            
-                            <h3 className="text-3xl  text-blue-900 font-bold">Since 1960</h3>
-                            <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>Lorem Ipsum</p>
-                            </div>
-                            
-                            <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>Lorem Ipsum</p>
-                            </div>
-                            <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
-                                Contact us
-                                <svg className="w-4 h-4 ml-1 group-hover: translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id="about">
+        <div className="my-4 py-4">
+          <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
+            Our History
+          </h2>
+          <div className="flex justify-center">
+            <div className="w-24 border-b-4 border-blue-900"></div>
+          </div>
+          <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">
+            About Q&P Cleaners
+          </h2>
+        </div>
+
+        <div
+          className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left"
+          data-aos="fade-up"
+        >
+          <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+            <img alt="card img" className="rounded-t float-right" src={img} />
+          </div>
+          <div
+            className="flex-col my-4 text-left lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8"
+            data-aos="zoom-in"
+            data-aos-delay="500"
+          >
+            <h3 className="text-3xl  text-blue-900 font-bold">Since 1960</h3>
+            <div>
+              <p className="my-5 text-l text-gray-600 font-semibold">
+                For more than six decades and spanning three generations, Q&P
+                Cleaners has been a trusted partner to renowned brands,
+                consistently delivering exceptional quality and price to our
+                cherished customers. As a privately held, family-owned
+                establishment, our unwavering commitment has always been
+                centered on tailoring our services to meet your unique clothing
+                care needs, whether it involves the meticulous removal of a
+                minor stain or the delicate alteration of a cherished wedding
+                gown.
+              </p>
+            </div>
+
+            <div>
+              <p className="my-5 text-l text-gray-600 font-semibold">
+                At Q&P Cleaners, Han Joon Lee and his business takes
+                responsibility towards the environment seriously. We are proud
+                to hold the distinction of being the first in all of Canada to
+                acquire the innovative SOLVONK4 machine. This state-of-the-art
+                technology employs a halogen-free solvent that is not only
+                non-toxic and non-hazardous but also biodegradable. Its
+                remarkable cleaning efficiency ensures that your garments are
+                treated with the utmost care while leaving a minimal ecological
+                footprint.
+              </p>
+            </div>
+
+            <div>
+              <p className="my-5 text-l text-gray-600 font-semibold">
+                Join us on a journey spanning over six decades, where tradition,
+                innovation, and a deep commitment to customer satisfaction have
+                always been at the heart of Q&P Cleaners.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Intro;
